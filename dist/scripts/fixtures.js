c744ee66656f854bbca41fa0af117b2984b04f71
Adam Forrester <adam@grapedesignllc.com>
2015-12-24 17:37:23 -0600

    Checkpoint 10 Commit

---
+++ dist/scripts/fixtures.js
@@ -0,0 +1,45 @@
+ var albumPicasso = {
+     name: 'The Colors',
+     artist: 'Pablo Picasso',
+     label: 'Cubism',
+     year: '1881',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+         { name: 'Blue', length: 161.71, audioUrl: '/assets/music/blue' },
+         { name: 'Green', length: 103.96, audioUrl: '/assets/music/green' },
+         { name: 'Red', length: 268.45, audioUrl: '/assets/music/red' },
+         { name: 'Pink', length: 153.14, audioUrl: '/assets/music/pink' },
+         { name: 'Magenta', length: 374.22, audioUrl: '/assets/music/magenta' }
+     ]
+ };
+ 
+
+ var albumMarconi = {
+     name: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { name: 'Hello, Operator?', length: '1:01' },
+         { name: 'Ring, ring, ring', length: '5:01' },
+         { name: 'Fits in your pocket', length: '3:21'},
+         { name: 'Can you hear me now?', length: '3:14' },
+         { name: 'Wrong phone number', length: '2:15'}
+     ]
+ };
+
+ var albumTool = {
+     name: 'Ænima',
+     artist: 'Tool',
+     label: 'Zoo Entertainment',
+     year: '1996',
+     albumArtUrl: 'assets/images/album_covers/tool-album-cover.jpg',
+     songs: [
+         { name: 'Stinkfish', length: '5:11' },
+         { name: 'Eulogy', length: '8:29' },
+         { name: 'H.', length: '6:07'},
+         { name: 'Useful Idiot', length: '0:39' },
+         { name: 'Forty Six & 2', length: '6:04'}
+     ]
+ };
